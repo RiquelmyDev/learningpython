08ed7a826958703da18bd9432ee0c6150afdd09f
Riquelmy Vasconcelos <119415461+RiquelmyDev@users.noreply.github.com>
2024-01-27 22:41:00 -0300

    Update entradada_de_dados.py

--- Mudulo_1/tipos_variaveis/entradada_de_dados.py
+++ Mudulo_1/tipos_variaveis/entradada_de_dados.py
@@ -11,3 +11,20 @@ else:
 
 mensagem = "Cara, voce ja tem 18 anos, entao voce pode tirar a porra da sua carteira de habilitação" if idade >= 18 else "Você não pode tirar a carteira de habilitação!"
 print (mensagem)
+
+
+
+
+
+
+
+champions = "Real madrid"
+libertadores = "Idependente"
+mls = "Inter Miami"
+
+
+time = int(input  ("Teste seu conhecimento no futebol, respondendo umas perguntas. Pronto?"))
+
+campeao = input (Qual maior campeao da libertadores?)
+if libertadores == "Real Madrid"
+  print ("isso ai, voce esta certo!")
